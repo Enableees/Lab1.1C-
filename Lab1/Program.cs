c42289c3f4bb1df9aed251989ec1e3f732130ca2
Enableees <vladshumkov@bk.ru>
2026-03-02 20:44:13 +0800

    Код до декомпозиции

---
+++ Lab1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int numb = int.Parse(name);
+            string outMessage = "";
+
+            if ((1 <= numb) & (numb <= 9999))
+            {
+                int s = numb / 100;
+                int f = numb % 100;
+                outMessage = $"{s} rubles {f} kopecks";
+            }
+            else
+            {
+                outMessage = "Error";
+            }
+
+            Console.WriteLine(outMessage);
+        }
+    }
+}
